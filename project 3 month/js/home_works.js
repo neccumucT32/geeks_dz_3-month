fc7a6dc73e0afbc424da41c0b09aa5d2974c0b18
Владислав <manchelaev25062001@gmail.com>
2024-11-05 18:42:04 +0600

    Update home_works.js

--- project 3 month/js/home_works.js
+++ project 3 month/js/home_works.js
@@ -1,4 +1,4 @@
-// 1.
+// 1. DZ#1
 const gmailButton = document.querySelector("#gmail_button");
 const gmailInput = document.querySelector("#gmail_input");
 const gmailResult = document.querySelector("#gmail_result");
@@ -36,3 +36,66 @@ const moveChild = () => {
 };
 
 moveChild();
+// 1. DZ#2
+const parentBlock = document.querySelector(".parent_block");
+const childBlock = document.querySelector(".child_block");
+
+let positionX = 0,
+  positionY = 0;
+
+// console.log( parentBlock.offsetWidth - childBlock.offsetWidth);
+
+const offsetWidth = parentBlock.offsetWidth - childBlock.offsetWidth;
+const offsetHeight = parentBlock.offsetHeight - childBlock.offsetHeight;
+
+const moveBlock = () => {
+  if (positionX < offsetWidth && positionY === 0) {
+    positionX++;
+    childBlock.style.left = `${positionX}px`;
+    requestAnimationFrame(moveBlock);
+  } else if (positionX >= offsetWidth && positionY < offsetHeight) {
+    positionY++;
+    childBlock.style.top = `${positionY}px`;
+    requestAnimationFrame(moveBlock);
+  } else if (positionY >= offsetHeight && positionX > 0) {
+    positionX--;
+    childBlock.style.left = `${positionX}px`;
+    requestAnimationFrame(moveBlock);
+  } else if (positionX === 0 && positionY > 0) {
+    positionY--;
+    childBlock.style.top = `${positionY}px`;
+    requestAnimationFrame(moveBlock);
+  }
+};
+moveBlock();
+
+// 2. 
+const btnStart = document.getElementById("start");
+const btnStop = document.getElementById("stop");
+const btnReset = document.getElementById("reset");
+const secondsDisplay = document.getElementById("seconds");
+
+let counter = 0;
+let intervalId = null;
+
+function startTimer() {
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      counter++;
+      secondsDisplay.textContent = counter;
+    }, 1000);
+  }
+}
+function stopTimer() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+function resetTimer() {
+  stopTimer();
+  counter = 0;
+  secondsDisplay.textContent = counter;
+}
+
+btnStart.addEventListener("click", startTimer);
+btnStop.addEventListener("click", stopTimer);
+btnReset.addEventListener("click", resetTimer);
