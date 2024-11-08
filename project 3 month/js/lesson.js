ad08d2093733bbe8ed5fa68eec75f51e4e9f7beb
Владислав <manchelaev25062001@gmail.com>
2024-11-08 18:23:21 +0600

    Update lesson.js

--- project 3 month/js/lesson.js
+++ project 3 month/js/lesson.js
@@ -41,3 +41,47 @@ console.log(numbers.replace(regExp, "*"));
 //     phoneResult.style.color = "red";
 //   }
 // };
+
+// TAB SLIDER DZ#3
+
+const tabContentBlocks = document.querySelectorAll(".tab_content_block");
+const tabs = document.querySelectorAll(".tab_content_item");
+const tabsParent = document.querySelector(".tab_content_items");
+let currentTabIndex = 0;
+
+const hideTabContent = () => {
+  tabContentBlocks.forEach((block) => {
+    block.style.display = "none";
+  });
+  tabs.forEach((tab) => {
+    tab.classList.remove("tab_content_item_active");
+  });
+};
+
+const showTabContent = (id = 0) => {
+  tabContentBlocks[id].style.display = "block";
+  tabs[id].classList.add("tab_content_item_active");
+};
+
+hideTabContent();
+showTabContent(currentTabIndex);
+
+tabsParent.onclick = (event) => {
+  if (event.target.classList.contains("tab_content_item")) {
+    tabs.forEach((tab, tabIndex) => {
+      if (event.target === tab) {
+        hideTabContent();
+        showTabContent(tabIndex);
+        currentTabIndex = tabIndex;
+      }
+    });
+  }
+};
+
+setInterval(() => {
+  currentTabIndex = (currentTabIndex + 1) % tabs.length;
+  hideTabContent();
+  showTabContent(currentTabIndex);
+}, 3000);
+
+
